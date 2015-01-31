49f2fa0c788e2058385f3a15582bd78599725bf1
medali.bettaieb <medali.bettaieb@esprit.tn>
2015-01-31 16:48:53 +0100

    config entity
    Signed-off-by: medali.bettaieb <medali.bettaieb@esprit.tn>

---
+++ tn.edu.esprit.cs.playBox-ejb/ejbModule/domain/User.java
@@ -0,0 +1,42 @@
+package domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Id
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
