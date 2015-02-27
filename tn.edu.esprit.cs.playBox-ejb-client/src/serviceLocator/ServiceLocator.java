4c6acf02c67272997d13f0c2aa224c6f4984b0f9
medali.bettaieb <medali.bettaieb@esprit.tn>
2015-02-27 20:40:41 +0100

    Signed-off-by: medali.bettaieb <medali.bettaieb@esprit.tn>

--- tn.edu.esprit.cs.playBox-ejb-client/src/serviceLocator/ServiceLocator.java
+++ tn.edu.esprit.cs.playBox-ejb-client/src/serviceLocator/ServiceLocator.java
@@ -12,53 +12,36 @@ public class ServiceLocator {
 	private static Map<String, Object> map;
 	private static Context context;
 
-	public  Object getProxy(String jndiName) {
-		Object object = null;
-		if (map.containsKey(jndiName)) {
-			object = map.get(jndiName);
-		} else {
+	public synchronized Object getProxy(String jndiName) {
+		Object proxy = null;
+		proxy = map.get(jndiName);
+		if (proxy == null) {
 			try {
-				object = context.lookup(jndiName);
-				map.put(jndiName, object);
+				proxy = context.lookup(jndiName);
 			} catch (NamingException e) {
 				e.printStackTrace();
 			}
+			map.put(jndiName, proxy);
 		}
-		return object;
+
+		return proxy;
 
 	}
 
 	private ServiceLocator() {
+		map = new HashMap<>();
 		try {
 			context = new InitialContext();
-			map = new HashMap<>();
 		} catch (NamingException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	public static ServiceLocator getInstance() {
+	public synchronized static ServiceLocator getInstance() {
 		if (instance == null) {
 			instance = new ServiceLocator();
 		}
 		return instance;
-	}
-
-	public Map<String, Object> getMap() {
-		return map;
-	}
-
-	public void setMap(Map<String, Object> map) {
-		this.map = map;
-	}
-
-	public Context getContext() {
-		return context;
-	}
-
-	public void setContext(Context context) {
-		this.context = context;
 	}
 
 }
